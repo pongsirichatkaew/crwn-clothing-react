2286abfba32e17439f9232aa5d381ac9485e8996
Pongsiri Chatkaew <pongsiri.ch@inet.co.th>
2021-09-02 08:11:44 +0700

    test

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -19,7 +19,7 @@ const Homepage = () => {
         <div className='menu-item'>
           <div className='content'>
             <h1 className='title'>SNEAKERS</h1>
-            <span className='subtitle'>SHOP NoW</span>
+            <span className='subtitle'>SHOP NOW</span>
           </div>
         </div>
         <div className='menu-item'>
