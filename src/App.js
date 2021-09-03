ebc44936d2e6a613d976cb0400bf9a2f9a652ca4
Pongsiri Chatkaew <pongsiri.ch@inet.co.th>
2021-09-03 08:15:21 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Homepage from './pages/homepage/homepage.component';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+// Switch wll return only route with noting else
+// prevent from accidently render multiple components
+const HatsPage = () => (
+  <div>
+    <h1>HAT PAGE</h1>
+  </div>
+);
 function App() {
   return (
     <div className='App'>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import './menu-item.styles.scss';
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      className='background-image'
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    ></div> 
-    <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
-    </div>
-  </div>
-);
 
-export default MenuItem;
+// HOC is a function that take component and modified it someway
+// turn in to new power component
+import { withRouter } from 'react-router';
+
+import './menu-item.styles.scss';
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+
+  return (
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        className='background-image'
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(MenuItem);
