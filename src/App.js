350cadd0aa83b30acb0866bf1683e558589cd7e0
Pongsiri Chatkaew <pongsiri.ch@inet.co.th>
2021-09-04 11:21:05 +0700

    header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 // Switch wll return only route with noting else
@@ -8,6 +9,7 @@ import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
