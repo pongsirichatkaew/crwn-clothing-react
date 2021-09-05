55e88893a281dd23f6d91be5c9adafbce04a9a58
Pongsiri Chatkaew <pongsiri.ch@inet.co.th>
2021-09-05 15:56:58 +0700

    add checkout page checkout item compoentn

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.urtils';
 
@@ -46,6 +47,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             path='/signin'
             render={() => {
